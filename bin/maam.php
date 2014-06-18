e4b0e9aed0018fa0b459a5a8c9e245cd76f7c255
Michael Moussa <michael.moussa@gmail.com>
2014-06-17 20:02:55 -0400

    Adding newline to the error messages

--- bin/maam.php
+++ bin/maam.php
@@ -6,9 +6,9 @@ use Moose\Maam\Generator\Generator;
 include __DIR__ . '/../vendor/autoload.php';
 
 if (!isset($argv[1])) {
-    die('Missing source path!');
+    die("Missing source path!\n");
 } elseif (!is_dir($argv[1])) {
-    die('The provided source path was not found');
+    die("The provided source path was not found\n");
 }
 
 $classMap = [];
